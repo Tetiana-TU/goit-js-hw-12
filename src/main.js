1da3f0ae255d97a41a7480f10fd7a9c283498b96
Dmytryienko_Tetiana <dmitrienko_tanya@ukr.net>
2025-01-23 20:01:28 +0200

    homework12

--- src/main.js
+++ src/main.js
@@ -8,16 +8,19 @@ import { fetchPhotosByQuery } from './js/pixabay-api';
 const searchFormEl = document.querySelector('.js-search-form');
 const galleryEl = document.querySelector('.js-gallery');
 const loader = document.querySelector('.loader');
-const loadMoreBtnEl = document.querySelector('.js-load-more-btn');
+const loadMoreBtnEl = document.querySelector('.load-more-btn');
 
 let page = 1;
 let searchedQuery = '';
+
 const onSearchFormSubmit = async event => {
   try {
     event.preventDefault();
+
     searchedQuery = event.currentTarget.elements.user_query.value.trim();
 
     if (searchedQuery === '') {
+      searchFormEl.reset();
       iziToast.error({
         message: 'Please enter your request',
         position: 'topRight',
@@ -33,6 +36,7 @@ const onSearchFormSubmit = async event => {
     const { data } = await fetchPhotosByQuery(searchedQuery, page);
 
     if (data.total === 0) {
+      searchFormEl.reset();
       iziToast.show({
         message:
           '"Sorry, there are no images matching your search query. Please try again!"',
@@ -40,23 +44,21 @@ const onSearchFormSubmit = async event => {
       });
 
       galleryEl.innerHTML = '';
-
+      searchFormEl.reset();
       return;
     }
     if (data.totalHits > 1) {
       loadMoreBtnEl.classList.remove('is-hidden');
-
       loadMoreBtnEl.addEventListener('click', onLoadMoreBtnClick);
     }
+
     const galleryTemplate = data.hits
       .map(el => createGalleryCardTemplate(el))
       .join('');
     galleryEl.innerHTML = galleryTemplate;
     loader.classList.add('is-hidden');
     scrollDown();
-    
     gallery.refresh();
-
     searchFormEl.reset();
   } catch {
     err => {
@@ -65,7 +67,6 @@ const onSearchFormSubmit = async event => {
         message: `${error}`,
         position: 'topRight',
       });
-      console.log(err);
     };
   }
 };
@@ -86,19 +87,17 @@ const onLoadMoreBtnClick = async event => {
     scrollDown();
     gallery.refresh();
 
-    if (page === data.totalHits) {
-     
-      loadMoreBtnEl.removeEventListener('click', onLoadMoreBtnClick);
+    if (page * 15 >= data.totalHits) {
       loadMoreBtnEl.classList.add('is-hidden');
+      loadMoreBtnEl.removeEventListener('click', onLoadMoreBtnClick);
+      loader.style.display = 'none';
     }
   } catch {
     err => {
-      loader.style.display = 'none';
       iziToast.show({
         message: `${error}`,
         position: 'topRight',
       });
-      console.log(err);
     };
   }
 };
